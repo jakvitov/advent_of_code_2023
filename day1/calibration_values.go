b17b5b1f94ffec53eb566a1b6a312050a7582ab3
Jakub Vítovec <jakvitov@gmail.com>
2023-12-05 23:13:42 +0100

    Initial commit - first challenge solved

---
+++ day1/calibration_values.go
@@ -0,0 +1,58 @@
+package day1
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+const INPUT_FILE string = "day1/test_input.txt"
+
+func readFileAsLines() []string {
+	result := make([]string, 0)
+	file, err := os.Open(INPUT_FILE)
+
+	if err != nil {
+		log.Fatal("Error while opening the file: " + INPUT_FILE + " " + err.Error())
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		result = append(result, scanner.Text())
+	}
+	return result
+}
+
+func isNum(char int32) bool {
+	return char > 47 && char < 58
+}
+
+func composeNum(first, second int32) int32 {
+	return (10 * first) + second
+}
+
+func decodeRow(input string) int32 {
+	//Suppose -1 cannot be encoded into utf-8 value
+	first := int32(-1)
+	last := int32(-1)
+	for _, char := range input {
+		if isNum(char) {
+			if first == -1 {
+				first = char - 48
+				last = first
+				continue
+			}
+			last = char - 48
+		}
+	}
+	return composeNum(first, last)
+}
+
+func DecodeFile() int64 {
+	result := int64(0)
+	lines := readFileAsLines()
+	for _, line := range lines {
+		result += int64(decodeRow(line))
+	}
+	return result
+}
---
+++ main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "advent_of_code_2023/day1"
+
+func main() {
+	println(day1.DecodeFile())
+}
