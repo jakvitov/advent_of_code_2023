e0c568eabb24cc7c811efcff3d11c0ace4f51cbd
Jakub Vítovec <jakvitov@gmail.com>
2023-12-10 18:09:06 +0100

    Day 9 second half

--- day9/sequence_extrapolation.go
+++ day9/sequence_extrapolation.go
@@ -29,12 +29,33 @@ func extrapolateSequence(input []int) int {
 	return input[len(input)-1] + extrapolateSequence(diffs)
 }
 
+// Same principle as before, just doing it backwards for the second half
+func extrapolateBackwards(input []int) int {
+	//Guard to end the recursion -> we end at constant array
+	isConstant := true
+	//List with the current differences
+	diffs := make([]int, len(input)-1)
+	for i := 1; i < len(input); i++ {
+		//We add to diffs the current difference
+		diffs[i-1] = input[i] - input[i-1]
+		//If our array is not constant, we break
+		if i > 2 && (diffs[i-1] != diffs[i-2]) {
+			isConstant = false
+		}
+	}
+	if isConstant {
+		//We end the recursion with extrapolating the last item as itself
+		return input[0] - diffs[0]
+	}
+	return input[0] - extrapolateBackwards(diffs)
+}
+
 func SumAllExtrapolatedValues() int {
 	lines := day1.ReadFileAsLines(INPUT_FILE)
 	result := 0
 	for _, line := range lines {
 		numbers := day5.TokenizeStringToInt(line, ' ')
-		result += extrapolateSequence(numbers)
+		result += extrapolateBackwards(numbers)
 	}
 	return result
 }
