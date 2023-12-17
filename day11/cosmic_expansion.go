c475c0fcc33a96df43b7d809520c961f20a7c8a2
jakvitov <jakvitov@gmail.com>
2023-12-17 20:31:28 +0100

    Day 11 solution

--- day11/cosmic_expansion.go
+++ day11/cosmic_expansion.go
@@ -3,10 +3,9 @@ package day11
 import (
 	"advent_of_code_2023/day1"
 	"advent_of_code_2023/day10"
-	"math"
 )
 
-const INPUT_TEXT string = "/home/jakub/Development/advent_of_code/advent_of_code_2023/day11/text_input.txt"
+const INPUT_TEXT string = "/home/jakub/Development/advent_of_code/advent_of_code_2023/day11/text_input2.txt"
 
 // Struct representing the space with galaxies
 type space struct {
@@ -16,14 +15,14 @@ type space struct {
 	galaxyCols map[int]bool
 }
 
-func max(int a, int b) int {
+func max(a, b int) int {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func min(int a, int b) int {
+func min(a, b int) int {
 	if a < b {
 		return a
 	}
@@ -33,7 +32,7 @@ func min(int a, int b) int {
 func parseGalaxiesAsMaze(input string) *space {
 	lines := day1.ReadFileAsLines(INPUT_TEXT)
 	result := &space{
-		data:     make([][]int32, len(input)),
+		data:     make([][]int32, len(lines)),
 		galaxies: make([]day10.Coord, 0),
 	}
 	for y, line := range lines {
@@ -49,24 +48,27 @@ func parseGalaxiesAsMaze(input string) *space {
 	result.galaxyRows = make(map[int]bool)
 	result.galaxyCols = make(map[int]bool)
 	for _, key := range result.galaxies {
-		result.galaxyRows[key.X] = true
-		result.galaxyCols[key.Y] = true
+		result.galaxyRows[key.Y] = true
+		result.galaxyCols[key.X] = true
 	}
 
 	return result
 }
 
-// Get additions for the coords due to space expansion
-func (s *space) getAdditions(first, second day10.Coord) int {
+func (s *space) getRowAdditions(first, second day10.Coord) int {
 	sum := 0
-	for i := min(first.X, second.X); i < max(first.X, second.X); i++ {
-		if !s.galaxyCols[i] {
+	for i := min(first.Y, second.Y); i < max(first.Y, second.Y); i++ {
+		if !s.galaxyRows[i] {
 			sum += 1
 		}
 	}
+	return sum
+}
 
-	for i := min(first.Y, second.Y); i < max(first.Y, second.Y); i++ {
-		if !s.galaxyRows[i] {
+func (s *space) getColumnAdditions(first, second day10.Coord) int {
+	sum := 0
+	for i := min(first.X, second.X); i < max(first.X, second.X); i++ {
+		if !s.galaxyCols[i] {
 			sum += 1
 		}
 	}
@@ -78,17 +80,18 @@ func (s *space) getGalaxyPairDistances() int {
 
 	//We iterate over all unique pairs
 	for i := 0; i < len(s.galaxies); i++ {
-		for k := i; k < len(s.galaxies); k++ {
+		for k := i + 1; k < len(s.galaxies); k++ {
 			first := &s.galaxies[i]
-			second := &s.galaxies[i]
-			xDiff := max(first.X, second.X) - min(first.X, second.X)
-			yDiff := max(first.Y, second.Y) - min(first.Y, second.Y)
-			dist := int(math.Sqrt(math.Pow(float64(xDiff), 2) + math.Pow(float64(yDiff), 2)))
-			dist += s.getAdditions(*first, *second)
+			second := &s.galaxies[k]
+			xDiff := (max(first.X, second.X) - min(first.X, second.X)) + s.getColumnAdditions(*first, *second)
+			yDiff := (max(first.Y, second.Y) - min(first.Y, second.Y)) + s.getRowAdditions(*first, *second)
+			//dist := int(math.Sqrt(math.Pow(float64(xDiff), 2) + math.Pow(float64(yDiff), 2)))
+			//Over which axis should we increment more due to space expansion
+			dist := xDiff + yDiff
+			//fmt.Printf("{%d.G}[%d:%d] -> {%d.G}[%d:%d] == %d\n", i+1, first.X, first.Y, k+1, second.X, second.Y, dist)
 			sum += dist
 		}
 	}
-
 	return sum
 }
 
--- main/main.go
+++ main/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "advent_of_code_2023/day10"
+import (
+	"advent_of_code_2023/day11"
+)
 
 func main() {
-	println(day10.GetFurthestNodeDistance())
+	println(day11.GetSumOfShortestPaths())
 }
